9a45360187311cdefb64abe679229f0c76bbab5d
Hussain Almakana <hussainalmakana@192-168-100-141.rev.bb.zain.com>
2024-12-06 01:29:44 +0300

    designing dashboard pages

--- EcoShop/Dashboard/DashboardViewController.swift
+++ EcoShop/Dashboard/DashboardViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class DashboardViewController: UIViewController {
+class DashboardViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
